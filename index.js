38c440a9073c1af0f3478d5afc05c70ab5e48760
ChenteJD <chentejardcruzz@gmail.com>
2025-09-30 22:11:47 -0600

    actualizacion del 30 de octubre

--- index.js
+++ index.js
@@ -66,13 +66,98 @@ app.get("/customers/getAll", async (req, res) => {
     res.json({ data: customers });
 });
 
+// GET PRODUCTS
+app.get("/products/getOne/:barcode", async (req, res) =>{
+    //Procesamiento de la peticion
+    const product=await Product.findOne({barcode:req.params.barcode}); //req.params.barcode obtiene el valor del parametro barcode
+    //Respuesta
+    if(product!=null){
+        res.json({data:product});
+    }else
+        res.status(404).json({
+            data:{message:"Product not found"}
+        });
+    
+});
+
+
+// GET CUSTOMERS
+app.get("/customers/getOne/:customer_id", async (req, res) =>{
+    //Procesamiento de la peticion
+    const customer=await Customer.findOne({customer_id:req.params.customer_id}); //req.params.customer_id obtiene el valor del parametro customer_id
+    //Respuesta
+    if(customer!=null){
+        res.json({data:customer});
+    }else
+        res.status(404).json({
+            data:{message:"Customer not found"}
+        });
+    
+});
+
+
+//UPDATE PRODUCT
+app.put("/products/updateOne/:barcode", async (req, res) =>{
+    //Procesamiento de la peticion
+   const productUpdated = await Product.updateOne({barcode:req.params.barcode},req.body); //req.params.barcode obtiene el valor del parametro barcode
+    //Respuesta
+   if(productUpdated !=null){
+       res.json({data:{message:"Product updated"} });
+   }else
+       res.status(404).json({
+           data:{message:"Product not found"}
+       });
+
+});
+
+//UPDATE CUSTOMERS
+app.put("/customers/updateOne/:customer_id", async (req, res) =>{
+    //Procesamiento de la peticion
+   const customerUpdated = await Customer.updateOne({customer_id:req.params.customer_id},req.body); //req.params.customer_id obtiene el valor del parametro customer_id
+    //Respuesta
+   if(customerUpdated !=null){
+       res.json({data:{message:"Customer updated"} });
+   }else
+       res.status(404).json({
+           data:{message:"Customer not found"}
+       });
+});
+
+//DELETED PRODUCT
+app.delete("/products/deleteOne/:barcode", async (req, res) =>{
+   //Procesamiento de la peticion
+    const productDeleted=await Product.findOneAndDelete({barcode:req.params.barcode});//req.params.barcode obtiene el valor del parametro barcode
+   //Respuesta
+    if(productDeleted!=null)
+        res.json({data:{message:"Product deleted", product:productDeleted} });
+    else
+        res.status(404).json({
+            data:{message:"Product not found"}
+        });
+});
+
+//DELETED CUSTOMER
+app.delete("/customers/deleteOne/:customer_id", async (req, res) =>{
+   //Procesamiento de la peticion
+    const customerDeleted=await Customer.findOneAndDelete({customer_id:req.params.customer_id});//req.params.customer_id obtiene el valor del parametro customer_id
+   //Respuesta
+    if(customerDeleted!=null)
+        res.json({data:{message:"Customer deleted", customer:customerDeleted} });
+    else
+        res.status(404).json({
+            data:{message:"Customer not found"}
+        });
+});   
 
 //Start server
-app.listen(app.get('port'),
-()=> console.log("Server listening on port" + app.get('port')));
+app.listen(app.get("port"), () =>{
+    console.log("Server listening on port" + app.get('port'))
+});
+
 
 
 //nodemon es para desarrollo, actualiza los cambios sin reiniciar el servidor
 //morgan es un middleware que muestra las peticiones que llegan al servidor
 //Mongoose es para interactuar con la base de datos MongoDB
-//async nos ayuda a mandar a traer muchas tareas y que se realizen al mismo tiempo, si no lo ponemos tenemos que esperar a que termine una tarea para iniciar la siguiente
+//async nos ayuda a mandar a traer muchas tareas y que se realizen al mismo tiempo,si no lo ponemos tenemos que esperar a que termine una tarea para iniciar la siguiente
+// findOneAndUpdate sirve para actualizar un documento y devolver el documento original o el modificado
